6e1c0ed531abad4c432b4f71d63be665b6f5e775
Iulian Ciornii <iulian.mail@gmail.com>
2018-05-18 20:40:38 +0300

    urmatorul commit

---
+++ lesson4/Oracle_Cloud_DB.sql
@@ -0,0 +1,16 @@
+
+
+
+select first_name,
+    salary +300
+from EMPLOYEES
+
+select 10+15-7+20*7 from dual
+
+/* comentariu pentru mai multe randuri */
+-- comentariu pentru un rand
+
+select EMAIL from employees 
+select distinct JOB_ID from employees 
+
+describe employees
